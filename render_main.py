4dff617b6c534084c016315e863288b0160152ac
mendy velner <meniwell@gmail.com>
2025-04-03 13:24:16 +0300

    fix

---
+++ render_main.py
@@ -0,0 +1,37 @@
+import warnings
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from routes.game_routes import games_router
+from routes.user_routes import user_router
+from routes.set_up_routes import setup_router
+import uvicorn
+from services.user_service import UserService
+import os
+
+warnings.simplefilter("ignore", category=DeprecationWarning)
+
+app = FastAPI()
+
+# הוספת middleware של CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# הוספת routers
+app.include_router(user_router)
+app.include_router(games_router)
+app.include_router(setup_router)
+
+
+@app.on_event("startup")
+async def startup():
+    UserService.deactivate_all_users()
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
